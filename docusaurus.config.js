22259c53cd7ba0649cc8a07eb0b4eb10c8d43e0b
chinhle <35798337+dangchinh25@users.noreply.github.com>
2024-09-09 21:38:41 -0700

    default dark mode (#30)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,6 +79,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
